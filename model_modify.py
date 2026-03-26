17b94c27dd7d88b1c69f8cb59c926494bf13a0d1
Stephan <stephan.semerad@gmail.com>
2026-03-26 16:34:58 +0400

    test

---
+++ model_modify.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, String, Integer, Float, text
+from model import engine
+
+# Example: add a new column to the existing SQLite table
+with engine.connect() as conn:
+    conn.execute(text("ALTER TABLE countries ADD COLUMN population INTEGER"))
+    conn.execute(text("ALTER TABLE countries ADD COLUMN is_eu Boolean"))
+    conn.commit()
