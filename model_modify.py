f557797a4e7536624c3fc7f5d8601af4988460f5
Stephan <stephan.semerad@gmail.com>
2026-04-02 17:26:21 +0400

    test

--- model_modify.py
+++ model_modify.py
@@ -3,8 +3,5 @@ from model import engine
 
 # Example: add a new column to the existing SQLite table
 with engine.connect() as conn:
-    conn.execute(text("ALTER TABLE countries ADD COLUMN capital STRING"))
-    conn.execute(text("ALTER TABLE countries ADD COLUMN capital_lat FLOAT"))
-    conn.execute(text("ALTER TABLE countries ADD COLUMN capital_lat FLOAT"))
-
+    conn.execute(text("ALTER TABLE countries ADD COLUMN capital_lng FLOAT"))
     conn.commit()
